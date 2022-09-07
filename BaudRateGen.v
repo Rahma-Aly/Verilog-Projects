b629165c8503a70d4c6da75522320e1fe7c3fbb5
Rahma-Aly <71734575+Rahma-Aly@users.noreply.github.com>
2022-09-07 05:53:23 +0200

    Update BaudRateGen.v

--- BaudRateGen.v
+++ BaudRateGen.v
@@ -1,38 +1,56 @@
 module BaudRateGen #(parameter clk_freq = 100000000, oversampling_rate = 16)(
 	input clk, 
 	input rst_n,
-	input [15:0] BaudRate, 
+	input [15:0] Divisor,  
 	output reg BCLK //BaudRate Clk
 );
-	reg [15:0] counter;
-	reg [15:0] Divisor;
-	    /*
+	    /* *
 	     * desired BR = Input clk freq /  (divisor x oversampling rate) 
 	     * oversampling rate -> 16 (usually) 
 	     * range of divisor: 1 to (2^16) -1 
 	     * freq = 100MHz
+	     * 
+	     *  n clk cycles = 1 UART clk cycle
 	     * */
-	always @(posedge clk or negedge rst_n) begin 
+	     
+	reg [15:0] count;
+	
+	always @(posedge clk or negedge rst_n) begin : Counter_Block
 	  if (!rst_n) begin
-	      counter = 16'b0; 
-	      end
-      else counter = counter + 16'b1; 
+	      count = 16'b0; 
+	  end
+      else count = count + 16'b1; 
 	end
 	
-	always @(posedge clk or negedge rst_n) begin
+	always @(posedge clk or negedge rst_n) begin : BaudRate_generation
 	    if (!rst_n) begin
 	        BCLK <= 1'b0;
-	        Divisor = clk_freq/(BaudRate*oversampling_rate);
-	        end
-	    else if (counter == Divisor) begin
-	        counter <= 16'b0;
-	        BCLK <= ~BCLK;
 	    end
-	    else
-	    BCLK <= BCLK;
+	   else if (is_Even(Divisor)) begin
+	        if (count == Divisor*oversampling_rate/2) begin
+	        count <= 16'b0;
+	        BCLK <= ~BCLK;
+	        end
+	        else BCLK <= BCLK;              
+       end
+       else begin
+            if (count == ((Divisor*oversampling_rate/2)+1)) begin
+            count <= 16'b0;
+            BCLK <= ~BCLK;
+            end
+            else BCLK <= BCLK; 
+           end
+           
 	end
 	
-	    
+function is_Even; // 0 -> odd and 1-> even
+    input [15:0] x;
+    begin
+        if (x %2 == 0) is_Even = 1;
+        else is_Even = 0;
+    end
+    endfunction	    
 	    
 	
 endmodule : BaudRateGen
+
